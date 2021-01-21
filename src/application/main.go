91eab2ab87418e063bada59e283d621d90916034
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2021-01-21 22:34:50 +0200

    default libraries lesson

---
+++ src/application/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func main() {
+	fortunateJson()
+}
+
+func fortunateJson() {
+	p1 := person{
+		First: "Foo",
+		Last:  "Bar",
+		Age:   32,
+	}
+
+	p2 := person{
+		First: "Hello",
+		Last:  "World",
+		Age:   42,
+	}
+
+	crowd := []person{p1, p2}
+
+	bs, err := json.Marshal(crowd)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(bs))
+
+	var crowd2 []person
+
+	err2 := json.Unmarshal(bs, &crowd2)
+
+	if err2 != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%+v", crowd2)
+}
